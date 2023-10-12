4fc394cd380efbd9ae2eca61da309d3929ce1a45
SunithaBisinigi <sunitabisinigi21@navgurukul.org>
2023-10-12 12:00:37 +0530

    footer done

---
+++ src/components/Footer.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <>
+        <div className='ft info'>
+            <div className='col ft1'>
+                <ul>
+                    <h3>Connect with Us</h3>
+                    <li>Get direction of office </li>
+                    <li> hr@qitsolution.co.in</li>
+                    <li> keyur@qitsolution.co.in</li>
+                </ul>
+            </div>
+            <div className='col ft2'>
+                <ul><h3>Navigation Links</h3>
+                    <li>About Us</li>
+                    <li>Products</li>
+                    <li>Business Solutions</li>
+                    <li>Career</li>
+                </ul>
+            </div>
+            <div className='col ft3'>
+                <ul><h3>Contact Us</h3>
+                    <addtress>
+                    Shop No: 370 & 350-351, 
+                        The Galleria,
+                        Near Sanjeev Kumar Auditorium,
+                        Surat, Gujarat 395009
+                    </addtress>
+                </ul>
+            </div>
+            <div className='col ft4'>
+                <ul><h3>Important Link</h3>
+                    <li>SAP Business B1 Partner in Surat</li>
+                    <li>SAP Business One Partner in Mumbai</li>
+                    <li>SAP Business B1 Partner in Bangalore</li>
+                    <li>SAP Business One Partner in Ahmedabad</li>
+                    <li>SAP Business One Partner in Vadodara</li>
+                </ul>
+            </div>
+        </div>
+        <div className='ft tag'>
+            <p>
+            © 2023 Quantum IT Solution. All Rights Reserved.
+            </p></div>
+    </>
+  )
+}
+
+export default Footer
